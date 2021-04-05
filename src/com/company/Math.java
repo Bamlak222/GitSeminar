3961ed74d3898939f25f19907c83a891411855ea
Bamlak <bamlak.hunegnaw0001@stud.hkr.se>
2021-04-05 23:17:36 +0200

    renaming

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -3,7 +3,7 @@ package com.company;
 public class Math {
 
     public static int Math(String[] args) {
-        public int add ( int i, int j){
+        public int addtion ( int i, int j){
             return i + j;
         }
     }
