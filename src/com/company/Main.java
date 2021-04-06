c98147040c31a179afb523545e91b4481e687d47
Bamlak <bamlak.hunegnaw0001@stud.hkr.se>
2021-04-06 12:52:14 +0200

    testing for conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
             public int multi ( int i, int j){
-                System.out.println("multiplication example result is ");
+                System.out.println("multiplication example result is as follows ");
                 return i * j;
             }
 
