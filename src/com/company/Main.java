7e2b373d7426629aae021295ccc129e6bfe0319f
Bamlak <bamlak.hunegnaw0001@stud.hkr.se>
2021-04-06 13:11:49 +0200

    Taste for merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
             public int multi ( int i, int j){
                 System.out.println("multiplication example result is as follows ");
+                System.out.println("merge test");
                 return i * j;
             }
 
