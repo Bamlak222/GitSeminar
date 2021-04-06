90e95164cd71d6dbc5fe630170961d37ba2ed41c
Bamlak <bamlak.hunegnaw0001@stud.hkr.se>
2021-04-06 09:29:17 +0200

    syntax corrected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static int main(String[] args) {
         public int sub ( int i, int j){
+            System.out.println(" the result is " );
             return i - j;
         }
     }
